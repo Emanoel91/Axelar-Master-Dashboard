be544c8ed5c13205692946992209fff8204f3ce9
Eman Raz <121406187+Emanoel91@users.noreply.github.com>
2025-09-20 01:52:57 +0330

    Update 1_🚀GMP_&_Token_Transfers.py

--- pages/1_🚀GMP_&_Token_Transfers.py
+++ pages/1_🚀GMP_&_Token_Transfers.py
@@ -424,11 +424,11 @@ donut_tx = px.pie(tx_df, names="Service", values="Count", color="Service", hole=
 )
 donut_tx.update_layout(showlegend=False)
 
-vol_df = pd.DataFrame({"Service": ["GMP", "Token Transfers"], "Volume": [total_gmp_vol, total_transfers_vol]})
+vol_df = pd.DataFrame({"Service": ["GMP", "Token Transfers"], "Volume": [total_gmp_vol, total_transfers_vol]}, textinfo='label+percent')
 donut_vol = px.pie(vol_df, names="Service", values="Volume", color="Service", hole=0.5, title="Share of Total Volume By Service", color_discrete_map={
         "GMP": "#ff7400",
         "Token Transfers": "#00a1f7"
-    }, textinfo='label+percent'
+    }
 )
 donut_tx.update_layout(showlegend=False)
 col5, col6 = st.columns(2)
