873c0f9882fcc9fc13d32f923d92c97c074807cb
Eman Raz <121406187+Emanoel91@users.noreply.github.com>
2025-09-22 18:32:45 +0330

    Update 8_🥩AXL_Staking.py

--- pages/8_🥩AXL_Staking.py
+++ pages/8_🥩AXL_Staking.py
@@ -7,36 +7,8 @@ from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.backends import default_backend
 
 # --- Page Config ------------------------------------------------------------------------------------------------------
-st.set_page_config(
-    page_title="Axelar Master Dashboard",
-    page_icon="https://axelarscan.io/logos/logo.png",
-    layout="wide"
-)
-
-# --- Title with Logo -----------------------------------------------------------------------------------------------------
-st.markdown(
-    """
-    <div style="display: flex; align-items: center; gap: 15px;">
-        <img src="https://axelarscan.io/logos/chains/axelarnet.svg" alt="Axelar Logo" style="width:60px; height:60px;">
-        <h1 style="margin: 0;">AXL Staking Stats</h1>
-    </div>
-    """,
-    unsafe_allow_html=True
-)
-
-# --- Builder Info ---------------------------------------------------------------------------------------------------------
-st.markdown(
-    """
-    <div style="margin-top: 20px; margin-bottom: 20px; font-size: 16px;">
-        <div style="display: flex; align-items: center; gap: 10px;">
-            <img src="https://pbs.twimg.com/profile_images/1841479747332608000/bindDGZQ_400x400.jpg" style="width:25px; height:25px; border-radius: 50%;">
-            <span>Built by: <a href="https://x.com/0xeman_raz" target="_blank">Eman Raz</a></span>
-        </div>
-    </div>
-    """,
-    unsafe_allow_html=True
-)
-
+st.set_page_config(page_title="Axelar Master Dashboard", page_icon="https://axelarscan.io/logos/logo.png", layout="wide")
+st.title("🥩AXL Staking")
 st.info("📊Charts initially display data for a default time range. Select a custom range to view results for your desired period.")
 st.info("⏳On-chain data retrieval may take a few moments. Please wait while the results load.")
 
