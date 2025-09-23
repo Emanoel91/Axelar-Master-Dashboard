dc15e862b65cb7bf96d4c259ec29084d3eca9f90
Eman Raz <121406187+Emanoel91@users.noreply.github.com>
2025-09-23 04:33:33 +0330

    Update 9_⛓Axelar_Network.py

--- pages/9_⛓Axelar_Network.py
+++ pages/9_⛓Axelar_Network.py
@@ -199,19 +199,7 @@ df_user_stats_tps = load_user_stats_tps()
 df_user_stats_success_rate = load_user_stats_success_rate()
 
 # === KPIs: Row 2 =========================================
-card_style = """
-    <div style="
-        background-color: #ffffff;
-        border: 1px solid #ffffff;
-        border-radius: 12px;
-        padding: 20px;
-        text-align: center;
-        box-shadow: 2px 2px 10px rgba(0,0,0,0.05);
-        ">
-        <h4 style="margin: 0; font-size: 20px; color: #000000;">{label}</h4>
-        <p style="margin: 5px 0 0; font-size: 20px; font-weight: bold; color: #000000;">{value}</p>
-    </div>
-"""
+
 
 col1, col2, col3, col4 = st.columns(4)
 with col1:
